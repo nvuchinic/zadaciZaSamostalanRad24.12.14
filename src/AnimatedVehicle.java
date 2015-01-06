1eee98636ca924e19187c537fae7420d41448d6c
nvuchinic <nermin.vucinic@gmail.com>
2015-01-06 18:34:55 +0100

    Update AnimatedVehicle.java

--- src/AnimatedVehicle.java
+++ src/AnimatedVehicle.java
@@ -15,7 +15,9 @@ protected int roadHeight;
 protected int minX;
 protected int maxX;
 	
-
+/**
+ * constructor method, for creating AnimatedVehicle objects
+ * */
 public AnimatedVehicle(int minX, int maxX, int roadHeight, double speed) {
 super(0, 0);
 this.minX = minX;
@@ -25,15 +27,24 @@ setSpeed(speed);
 resetPosition();
 }
 
+/**
+ * method for (re)setting x and y coordinates that are used for drawing the objects of type AnimatedVehicle
+ * */
 public void resetPosition() {
 setPosition((int) (minX + Math.random() * (maxX - minX)), (int)(-HEIGHT * Math.random() * 10));
 }
 
+/**
+ * method for setting the speed of objects of type AnimatedVehicle
+ * */
 @Override
 public void setSpeed(double speed) {
 this.speed = speed / 2;
 }
 
+/**
+ * method for animating objects of type AnimatedVehicle (changing the y coordinate for drawing the object)
+ * */
 @Override
 public void animateFrame(long frameNumber) {
 // move down or reset position if passed
@@ -43,12 +54,4 @@ y += speed;
 resetPosition();
 }
 }
-
-//@Override
-//public void draw(Graphics graphics) {
-//// draw car only if in visible area
-//if (y > -HEIGHT && y < roadHeight) {
-//drawCar(graphics, Color.BLACK);
-//}
-//}
 }
