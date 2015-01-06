7d10972df2da7f8e03aae05b5e4215c9f0fd6d19
nvuchinic <nermin.vucinic@gmail.com>
2015-01-06 17:42:22 +0100

    Update AnimatedGameArtifact.java

--- src/AnimatedGameArtifact.java
+++ src/AnimatedGameArtifact.java
@@ -1,3 +1,6 @@
+/**
+ * interface that extends GameArtifact inteface and contains signature for two methods 
+ * */
 public interface AnimatedGameArtifact extends GameArtifact {
 void animateFrame(long frameNumber);
 void setSpeed(double speed);
